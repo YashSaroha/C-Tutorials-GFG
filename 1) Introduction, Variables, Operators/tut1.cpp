672f6db7015c3b68fd484d4959d8b6064eaaa91c
unknown <yashsaroha966@gmail.com>
2023-06-25 17:51:19 +0530

    tut 1 added

---
+++ 1) Introduction, Variables, Operators/tut1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num = 3;
+    string name = "yash";
+    char gender ='M';
+    bool value = true;
+    float marks = 45;
+
+    cout << num << "\n";
+    cout << name << "\n";
+    cout << gender << "\n";
+    cout << value << "\n";
+    cout << marks << "\n";
+    return 0;
+}
