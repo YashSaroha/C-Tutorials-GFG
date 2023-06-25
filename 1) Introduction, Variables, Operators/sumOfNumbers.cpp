498f682bbb87b1134860d50ed0b05c9b07750fc7
unknown <yashsaroha966@gmail.com>
2023-06-25 17:50:56 +0530

    sum of numbers question added

---
+++ 1) Introduction, Variables, Operators/sumOfNumbers.cpp
@@ -0,0 +1,13 @@
+// sum of first n natural numbers
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n = 5;
+    // sum of first 5 natural numbers
+    int sum = n*(n+1)/2 ;
+    cout << sum ;
+
+    return 0;
+}
