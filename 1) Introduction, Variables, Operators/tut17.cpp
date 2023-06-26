db0c1bce3b4035e62388b78e447209745412978a
unknown <yashsaroha966@gmail.com>
2023-06-26 20:21:26 +0530

    tut 17 added

---
+++ 1) Introduction, Variables, Operators/tut17.cpp
@@ -0,0 +1,75 @@
+//                      TYPE CONVERSION
+
+#include<iostream>
+using namespace std;
+
+// int main()
+// {
+//     float x = 3.5;
+//     int y = x;
+//     bool z = y;
+
+//     cout << z;
+//     return 0;
+// }
+
+
+
+
+// **************************************************************************//
+
+
+
+
+// int main()
+// {
+//     int x = 10;
+//     char y = 'A';               // ascii value of A is 65
+//     cout << x+y << '\n';
+//     float z = 5.5;
+//     cout << x+z;
+//     return 0;
+// }
+
+
+
+// **************************************************************************//
+
+
+
+// int main()
+// {
+//     int x=15, y=2 ;
+//     double z = x/y;
+//     cout << z;
+//     return 0;
+// }
+
+
+
+// ******************************************************************************
+
+
+//            C STYLE EXPLICIT CONVERSION
+
+// int main()
+// {
+//     int x=15, y=2 ;
+//     double z = double (x)/y;
+//     cout << z;
+//     return 0;
+// }
+
+
+
+// *****************************************************************************
+
+//            C++ STYLE EXPLICIT CONVERSION 
+
+// int main()
+// {
+//     int x=15, y=2 ;
+//     double z = static_cast<double> (x)/y;
+//     cout << z;
+//     return 0;
+// }
