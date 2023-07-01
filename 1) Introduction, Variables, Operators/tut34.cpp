fcd0f9df3ecbe134b09667fd26f079d0a642bf9f
unknown <yashsaroha966@gmail.com>
2023-07-01 21:01:37 +0530

    tut 34 added

---
+++ 1) Introduction, Variables, Operators/tut34.cpp
@@ -0,0 +1,41 @@
+// PROBLEMS ON OPERATORS
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // 1)
+        // int x = 5;
+        // int z = (x==4 && x++) ;
+        // cout << z << " " << x ;
+
+    // 2)
+        // int x = 5;
+        // int z = (x==4 || x++) ;
+        // cout << z << " " << x ;
+
+    // 3)
+        // int x = 5;
+        // int z = (x==5 || x++) ;
+        // cout << z << " " << x ;
+
+    // 4)
+        // int x=1 , y=2 , z=3 , a;
+        // a = (x,y,z);
+        // cout << a;
+
+    // 5)
+        // int x=1 , y=2 , z=3 , a;
+        // a = x,y,z;
+        // cout << a;
+
+    // 6)
+        // int a=10 , b=20 , c=30;
+        // if (c>b>a)
+        // cout << "Y";
+        // else
+        // cout << "N";
+    
+    return 0;
+}
