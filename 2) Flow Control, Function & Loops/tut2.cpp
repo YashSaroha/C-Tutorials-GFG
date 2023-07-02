cd7890a3bb7837c34f2f01d9bc6f6ec8157bd4db
unknown <yashsaroha966@gmail.com>
2023-07-02 20:50:31 +0530

    tut 2 added

---
+++ 2) Flow Control, Function & Loops/tut2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x ;
+    cout << "Enter a number : ";
+    cin >> x ;
+    if (x%2 == 0)
+    {
+        cout << "Number is even";
+    }
+    else
+    {
+        cout << "Number is odd";
+    }
+    
+
+    return 0;
+}
