5a1d7bcf8306271dcea84e5d4cf8a2b3778a8c9d
unknown <yashsaroha966@gmail.com>
2023-07-03 20:28:59 +0530

    tut 10 added

---
+++ 2) Flow Control, Function & Loops/tut10.cpp
@@ -0,0 +1,52 @@
+// FUNCTIONS
+
+#include<iostream>
+using namespace std;
+
+
+// void fun()
+// {
+//     cout << "function fun() called \n";
+// }
+// int main()
+// {
+//     cout << "Before calling function \n";
+//     fun();
+//     cout << "After calling function \n";
+//     return 0;
+// }
+
+
+
+// int getMax(int a, int b)
+// {
+//     if (a>b)
+//     return a;
+//     else
+//     return b;
+// }
+// int main()
+// {
+//     int a=10 , b=20 ;
+//     cout << getMax(a,b) ;
+//     return 0;
+// }
+
+
+
+// void greetMsg()
+// {
+//     cout << "Hi\n";
+//     cout << "Welcome to c++ coding \n";
+// }
+// void exitMsg()
+// {
+//     cout << "Thanks, visit again";
+// }
+// int main()
+// {
+//     greetMsg();
+//     cout << "This is the main function \n";
+//     exitMsg();
+//     return 0;
+// }
