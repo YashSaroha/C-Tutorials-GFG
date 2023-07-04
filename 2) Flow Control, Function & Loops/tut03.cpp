d3d4292470c120785efeb4f7d6b1c49b7c371cef
unknown <yashsaroha966@gmail.com>
2023-07-04 21:26:42 +0530

    tut 03 added

---
+++ 2) Flow Control, Function & Loops/tut03.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std; 
+
+int main()
+{
+    int x;
+    cout << "Enter a number : ";
+    cin >> x ;
+
+    if (x>0)
+        cout << "Positive";
+    else if (x<0)
+        cout << "Negative";
+    else
+        cout << "Zero";
+    
+    return 0;
+}
