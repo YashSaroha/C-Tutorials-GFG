c5e5dc80548244caf2635ea33a49a82abedf3910
unknown <yashsaroha966@gmail.com>
2023-07-07 20:55:04 +0530

    tut 22 added

---
+++ 2) Flow Control, Function & Loops/tut22.cpp
@@ -0,0 +1,29 @@
+// WHILE LOOP
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int i=0 ;
+    // while (i<5)
+    // {
+    //     cout << "hello\n";
+    //     i++ ;
+    // }
+
+
+
+    // cout << "Enter a number : ";
+    // int i=1 , n ;
+    // cin >> n ;
+    // while (i<=n)
+    // {
+    //     cout << "Hello\n";
+    //     i++ ;
+    // }
+    // cout << "Bye";
+    
+
+    return 0;
+}
