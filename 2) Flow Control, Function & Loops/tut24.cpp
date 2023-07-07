fd4f3917a7008e345cbd84445eb057d0af28f551
unknown <yashsaroha966@gmail.com>
2023-07-07 20:55:48 +0530

    tut 24 added

---
+++ 2) Flow Control, Function & Loops/tut24.cpp
@@ -0,0 +1,33 @@
+// BREAK STATEMENT
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int n;
+    // cout << "Enter a number : ";
+    // cin >> n ;
+    // for (int i=2 ; i<=n ; i++)
+    // {
+    //     if ( n%i == 0 )
+    //     {   
+    //         cout << "Smallest divisor greater than 1 is : " << i ;
+    //         break;
+    //     }
+    // }
+    
+
+
+    // int i = 1;
+    // while (i<=5)
+    // {
+    //     if( i==3 )
+    //     break;
+    //     cout << i << " ";
+    //     i++ ;
+    // }
+    // cout << i << " ";
+
+    return 0;
+}
