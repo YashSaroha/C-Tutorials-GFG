b3fa12174c1a34b5ab08e0fa9e8d88dc1689e7ff
unknown <yashsaroha966@gmail.com>
2023-07-07 20:54:40 +0530

    tut 21 added

---
+++ 2) Flow Control, Function & Loops/tut21.cpp
@@ -0,0 +1,18 @@
+// DO WHILE LOOP
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=5;
+    do
+    {
+        cout << "hello\n";
+        i++ ; 
+    } while (i<3);
+    
+    return 0;
+}
+
+// even the condition is false, the code will execute atleast once and then condition is checked
