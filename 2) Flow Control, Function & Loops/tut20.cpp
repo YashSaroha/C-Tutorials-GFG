6f667282ca5c95d3088187684df589803dedf34e
unknown <yashsaroha966@gmail.com>
2023-07-07 20:54:21 +0530

    tut 20 added

---
+++ 2) Flow Control, Function & Loops/tut20.cpp
@@ -0,0 +1,41 @@
+// FOR LOOP
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // for(int i=0 ; i<3 ; i++)
+    // {
+    //     cout << "yash" << " " << i << endl ;
+    // }
+
+
+
+    // for(int i=0 ; i<3 ; i++)
+    // cout << "yash" << " " << i << endl ;
+    // cout << i ;
+    // this will give error as the variable i declared inside the loop is not accessible outside the loop
+
+
+
+    // int i;
+    // for ( i=0 ; i<3 ; i++ )
+    // cout << "yash" << " " << i << endl ;
+    // cout << i ;
+    // as i was declared outside loop, it will not give error
+
+
+
+    // for (int i=0 ; i!=10 ; i+=2)
+    // cout << "yash\n";
+    // this will go in infinite loop as condition is never satisfied
+    
+    
+
+    // for ( ; ; )
+    // cout << "hello\n";
+    // this will go in infinite loop as no condition is considered as true
+
+    return 0;
+}
