b786952deb5d30ea9f7825786052f348b3efc416
unknown <yashsaroha966@gmail.com>
2023-07-08 19:58:24 +0530

    tut 28 added

---
+++ 2) Flow Control, Function & Loops/tut28.cpp
@@ -0,0 +1,22 @@
+// TOTAL DIGITS IN A GIVEN NUMBER
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int count=0 ;
+    cout << "Enter a number : ";
+    cin >> n;
+    
+    while (n>0)
+    {
+        n = n/10 ;
+        count++ ;
+    }
+
+    cout << count ; 
+
+    return 0;
+}
