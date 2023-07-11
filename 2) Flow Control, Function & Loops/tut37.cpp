8726d4ac0b1e57c33e8e7655f6e4c6260c476175
unknown <yashsaroha966@gmail.com>
2023-07-11 13:34:59 +0530

    tut 37 added

---
+++ 2) Flow Control, Function & Loops/tut37.cpp
@@ -0,0 +1,22 @@
+// DECIMAL TO BINARY
+
+#include <iostream>
+using namespace std;
+void DecimalToBinary(int n) {
+   int binaryNumber[100], num=n;
+   int i = 0;
+   while (n > 0) {
+      binaryNumber[i] = n % 2;
+      n = n / 2;
+      i++;
+   }
+   cout<<"Binary form of "<<num<<" is ";
+   for (int j = i - 1; j >= 0; j--)
+   cout << binaryNumber[j];
+   cout<<endl;
+}
+int main() {
+   DecimalToBinary(88);
+   return 0;
+}
+
