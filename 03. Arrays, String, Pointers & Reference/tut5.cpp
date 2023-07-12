7ee01fd336b2c2deaea2739eb32f120621ee6c17
unknown <yashsaroha966@gmail.com>
2023-07-12 21:09:33 +0530

    tut 5 added

---
+++ 03. Arrays, String, Pointers & Reference/tut5.cpp
@@ -0,0 +1,12 @@
+// DIFFERENT TYPES OF ARRAYS
+
+//   1)  FIXED SIZE ARRAY
+//          # Allocated in function call stack
+//                  int arr[100] ;            
+//                  int arr[n] ;           
+//                  int arr[]  = {1,2,3,4} ;          
+//          # Allocated on heap
+//                  int *arr = new int[n];
+
+//   2)  DYNAMIC SIZE ARRAY
+//            vector in c++ STL
